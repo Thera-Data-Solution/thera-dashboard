9170e64c0e184eb77a3dadc2d6c394661cd4cd69
kepsgurih <kepsgurih@pm.me>
2025-11-05 15:30:51 +0700

    :green_heart: first try prod

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,19 @@
 import axios, { AxiosError } from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const TENANT_ID = import.meta.env.VITE_TENANT;
+
+const headers: Record<string, string> = {
+  "Content-Type": "application/json",
+};
+
+if (TENANT_ID) {
+  headers["x-tenant-id"] = TENANT_ID;
+}
 
 const authApi = axios.create({
   baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "x-tenant-id": import.meta.env.VITE_TENANT,
-  },
+  headers
 });
 
 export { AxiosError, authApi };
