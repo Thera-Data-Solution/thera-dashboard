5ca7825e74d5ecdcbf3bd1d3b78bad882b6f7091
kepsgurih <kepsgurih@pm.me>
2025-11-05 15:52:47 +0700

    :wrench: modify header

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,18 @@
 import axios, { AxiosError } from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
-const TENANT_ID = import.meta.env.VITE_TENANT;
+
+const getTenantId = () => {
+  const host = window.location.hostname;
+
+  if (host.includes("localhost")) return "c78ae261-d987-4570-9220-774846531ddf";
+  if (host.includes("theravickya")) return "c78ae261-d987-4570-9220-774846531ddf";
+  if (host.includes("breathworkalchemy")) return "788";
+
+  return null;
+};
+
+const TENANT_ID = getTenantId();
 
 const headers: Record<string, string> = {
   "Content-Type": "application/json",
@@ -14,7 +25,7 @@ if (TENANT_ID) {
 
 const authApi = axios.create({
   baseURL: API_BASE_URL,
-  headers
+  headers,
 });
 
 export { AxiosError, authApi };
